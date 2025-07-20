8ebd2de2489161de28f0a667c3084feda679e400
pradeep_punnu <k.pradeep0491@gmail.com>
2025-07-20 22:08:30 +0530

    first commit

---
+++ kafka-workspace/kafka-producer-example/src/main/java/com/test/service/KafkaMessagePublisher.java
@@ -0,0 +1,33 @@
+package com.test.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaMessagePublisher(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessageToTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("javatechie-demo3", message);
+        //future.get() //it will slow down the process
+
+        //callback implementation
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        message + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+
+}
